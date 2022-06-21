1be808f8db3b4520c085828882f69cd8cb8f80c8
langoclinh2312 <vnbboy12345@gmail.com>
2022-06-21 13:58:17 +0700

    push code

---
+++ Setup/Patch/Data/AddCompanyTypeCustomerAttribute.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace AHT\AttributeCustomer\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Config;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddCompanyTypeCustomerAttribute implements DataPatchInterface
+{
+
+    /**
+     * @var CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $setup;
+
+    /**
+     * @var Config
+     */
+    private $eavConfig;
+
+    /**
+     * AccountPurposeCustomerAttribute constructor.
+     * @param ModuleDataSetupInterface $setup
+     * @param Config $eavConfig
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $setup,
+        Config $eavConfig,
+        CustomerSetupFactory $customerSetupFactory
+    ) {
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->setup = $setup;
+        $this->eavConfig = $eavConfig;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->setup]);
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType(\Magento\Customer\Model\Customer::ENTITY);
+        $attributeSetId = $customerSetup->getDefaultAttributeSetId($customerEntity->getEntityTypeId());
+        $attributeGroup = $customerSetup->getDefaultAttributeGroupId($customerEntity->getEntityTypeId(), $attributeSetId);
+        $customerSetup->addAttribute(\Magento\Customer\Model\Customer::ENTITY, 'company_type', [
+            'type' => 'varchar',
+            'label' => 'Company Type',
+            'input' => 'select',
+            'required' => false,
+            'default' => null,
+            'user_defined' => true,
+            'unique' => false,
+            'visible' => true,
+            'user_defined' => true,
+            'system' => false,
+            'is_visible_in_grid' => true,
+            'is_used_in_grid' => true,
+            'is_filterable_in_grid' => true,
+            'is_searchable_in_grid' => true
+        ]);
+        $newAttribute = $this->eavConfig->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'company_type');
+        $newAttribute->addData([
+            'used_in_forms' => ['adminhtml_customer', 'customer_account_edit', 'customer_account_create'],
+            'attribute_set_id' => $attributeSetId,
+            'attribute_group_id' => $attributeGroup
+        ]);
+        $newAttribute->save();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    public static function getVersion()
+    {
+        return [];
+    }
+}
